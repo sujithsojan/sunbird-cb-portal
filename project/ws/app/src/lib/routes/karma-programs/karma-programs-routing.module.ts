13380969451eacaef7d788cf9a94083a67182aac
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-08-01 13:00:20 +0530

    Make v2 is default

--- project/ws/app/src/lib/routes/karma-programs/karma-programs-routing.module.ts
+++ project/ws/app/src/lib/routes/karma-programs/karma-programs-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
-import { KarmaProgramsFormV1Service } from './service/karma-programs-form-v1.service'
+// import { KarmaProgramsFormV1Service } from './service/karma-programs-form-v1.service'
 import { KarmaProgramsComponent } from './karma-programs/karma-programs.component'
-import { KarmaProgramsMicrositeV1Component } from './karma-programs-microsite-v1/karma-programs-microsite-v1.component'
+// import { KarmaProgramsMicrositeV1Component } from './karma-programs-microsite-v1/karma-programs-microsite-v1.component'
 import { KarmaProgramDataService } from './service/karma-program-data.service'
 import { KarmaProgramsMicrositeV2Component } from './karma-programs-microsite-v2/karma-programs-microsite-v2.component'
 import { KarmaProgramsFormV2Service } from './service/karma-programs-form-v2.service.'
@@ -24,19 +24,19 @@ const routes: Routes = [
             programData: KarmaProgramDataService,
         },
     },
+    // {
+    //     path: ':programName/:playListKey/:orgId/micro-sites',
+    //     component: KarmaProgramsMicrositeV1Component,
+    //     data: {
+    //         pageId: ':programName/:playListKey/:orgId/micro-sites',
+    //         module: 'Learn',
+    //     },
+    //     resolve: {
+    //         formData: KarmaProgramsFormV1Service,
+    //     },
+    // },
     {
         path: ':programName/:playListKey/:orgId/micro-sites',
-        component: KarmaProgramsMicrositeV1Component,
-        data: {
-            pageId: ':programName/:playListKey/:orgId/micro-sites',
-            module: 'Learn',
-        },
-        resolve: {
-            formData: KarmaProgramsFormV1Service,
-        },
-    },
-    {
-        path: ':programName/:playListKey/:orgId/micro-sites/v2',
         component: KarmaProgramsMicrositeV2Component,
         data: {
             pageId: ':programName/:playListKey/:orgId/micro-sites',
