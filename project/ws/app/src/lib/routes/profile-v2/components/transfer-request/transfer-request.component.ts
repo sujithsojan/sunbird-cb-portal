48ccfef573796711e3e31e621436c019f7174740
nitinraj-tarento <nitin.raj@tarento.com>
2024-08-13 00:32:13 +0530

    transfer request search for org and designation fix

--- project/ws/app/src/lib/routes/profile-v2/components/transfer-request/transfer-request.component.ts
+++ project/ws/app/src/lib/routes/profile-v2/components/transfer-request/transfer-request.component.ts
@@ -46,16 +46,16 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
     }
 
     this.transferRequestForm.get('organization')!.valueChanges
-    .subscribe((value: string) => {
-      if (value !== this.data.portalProfile.employmentDetails.departmentName) {
-        this.otherDetails = true
-      } else {
-        this.otherDetails = false
-      }
-    })
+      .subscribe((value: string) => {
+        if (value !== this.data.portalProfile.employmentDetails.departmentName) {
+          this.otherDetails = true
+        } else {
+          this.otherDetails = false
+        }
+      })
 
     if (this.transferRequestForm.get('organization')) {
-        this.transferRequestForm.get('organization')!.valueChanges
+      this.transferRequestForm.get('organization')!.valueChanges
         .pipe(
           debounceTime(250),
           distinctUntilChanged(),
@@ -63,16 +63,16 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
         )
         .subscribe(res => {
           if (res) {
-            this.deptFilterData = this.departmentData && this.departmentData.filter(item => item.toLowerCase().includes(res && res.toLowerCase()))
-          }
-          else {
+            this.deptFilterData = this.departmentData &&
+             this.departmentData.filter(item => item.toLowerCase().includes(res && res.toLowerCase()))
+          } else {
             this.deptFilterData = this.departmentData
           }
         })
-      }
+    }
 
-       if (this.transferRequestForm.get('designation')) {
-        this.transferRequestForm.get('designation')!.valueChanges
+    if (this.transferRequestForm.get('designation')) {
+      this.transferRequestForm.get('designation')!.valueChanges
         .pipe(
           debounceTime(250),
           distinctUntilChanged(),
@@ -80,17 +80,15 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
         )
         .subscribe(res => {
           if (res) {
-            // this.deptFilterData = this.data && this.data.designationsMeta.filter(item => item.toLowerCase().includes(res && res.toLowerCase()))
             const designonData = this.data && this.data.designationsMeta
             this.designationData = designonData.filter((val: any) =>
               val && val.name.toLowerCase().includes(res && res.toLowerCase())
             )
-          }
-          else {
+          } else {
             this.designationData = this.data && this.data.designationsMeta
           }
         })
-      }
+    }
   }
 
   ngOnInit() {
@@ -102,7 +100,7 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
   }
 
   organizationSearch(value: string) {
-      const filterVal = value.toLowerCase()
+    const filterVal = value.toLowerCase()
     return this.departmentData.filter(option => option.toLowerCase().includes(filterVal))
   }
 
@@ -133,31 +131,31 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
     }
     postData.request.profileDetails.professionalDetails.push(data)
     this.userProfileService.editProfileDetails(postData)
-    .pipe(takeUntil(this.destroySubject$))
-    .subscribe((_res: any) => {
-      this.matSnackBar.open('Your transfer request has been sent for approval')
-      // this.matSnackBar.open(this.handleTranslateTo('transferRequestSent'))
-      this.enableWithdraw.emit(true)
-      this.handleCloseModal()
-    },         (error: HttpErrorResponse) => {
-      if (!error.ok) {
-        this.matSnackBar.open(this.handleTranslateTo('transferRequestFailed'))
-      }
-    })
+      .pipe(takeUntil(this.destroySubject$))
+      .subscribe((_res: any) => {
+        this.matSnackBar.open('Your transfer request has been sent for approval')
+        // this.matSnackBar.open(this.handleTranslateTo('transferRequestSent'))
+        this.enableWithdraw.emit(true)
+        this.handleCloseModal()
+      },         (error: HttpErrorResponse) => {
+        if (!error.ok) {
+          this.matSnackBar.open(this.handleTranslateTo('transferRequestFailed'))
+        }
+      })
   }
 
   getAllDeptData(): void {
     this.userProfileService.getAllDepartments()
-    .pipe(takeUntil(this.destroySubject$))
-    .subscribe((res: any) => {
-      this.departmentData = res.sort((a: any, b: any) => {
-        return a.toLowerCase().localeCompare(b.toLowerCase())
+      .pipe(takeUntil(this.destroySubject$))
+      .subscribe((res: any) => {
+        this.departmentData = res.sort((a: any, b: any) => {
+          return a.toLowerCase().localeCompare(b.toLowerCase())
+        })
+      },         (error: HttpErrorResponse) => {
+        if (!error.ok) {
+          this.matSnackBar.open(this.handleTranslateTo('orgFetchDataFailed'))
+        }
       })
-    },         (error: HttpErrorResponse) => {
-      if (!error.ok) {
-        this.matSnackBar.open(this.handleTranslateTo('orgFetchDataFailed'))
-      }
-    })
   }
 
   handleTranslateTo(menuName: string): string {
