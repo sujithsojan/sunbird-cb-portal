cdbc0dbdaf416318b70317fe94e18ea082ebcde1
nitinraj-tarento <nitin.raj@tarento.com>
2024-09-04 12:53:44 +0530

    transfer request fix

--- project/ws/app/src/lib/routes/profile-v2/components/transfer-request/transfer-request.component.ts
+++ project/ws/app/src/lib/routes/profile-v2/components/transfer-request/transfer-request.component.ts
@@ -4,11 +4,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { HttpErrorResponse } from '@angular/common/http'
 import { MatSnackBar } from '@angular/material'
 
-import { Subject } from 'rxjs'
+// import { Observable, Subject } from 'rxjs'
 import { debounceTime, distinctUntilChanged, startWith, takeUntil } from 'rxjs/operators'
 
 import { UserProfileService } from '../../../user-profile/services/user-profile.service'
 import { ConfigurationsService } from '@sunbird-cb/utils-v2'
+import { Subject } from 'rxjs'
 
 @Component({
   selector: 'ws-transfer-request',
@@ -27,8 +28,10 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
   departmentData: any[] = []
   otherDetails = false
   deptFilterData: any[] = []
+  // deptFilterData  : Observable<string[]>
   designationData: any[] = []
   private destroySubject$ = new Subject()
+  isInValidOrgSelection:boolean = false
 
   constructor(
     public dialogRef: MatDialogRef<TransferRequestComponent>,
@@ -55,6 +58,7 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
       })
 
     if (this.transferRequestForm.get('organization')) {
+      console.log(this.transferRequestForm.get('organization')!.valueChanges, "this.transferRequestForm.get('organization')!.valueChanges")
       this.transferRequestForm.get('organization')!.valueChanges
         .pipe(
           debounceTime(250),
@@ -63,38 +67,85 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
         )
         .subscribe(res => {
           if (res) {
+            console.log(res, "response======")
             this.deptFilterData = this.departmentData &&
              this.departmentData.filter(item => item.toLowerCase().includes(res && res.toLowerCase()))
+             const orgSearchVal = this.transferRequestForm.controls['organization']
+             console.log(this.deptFilterData, "v-------------")
+             console.log(this.deptFilterData.length, "his.deptFilterData.length-------------")
+             if(this.deptFilterData && this.deptFilterData.length && this.deptFilterData.length > 0) {
+              
+              this.isInValidOrgSelection = false
+              orgSearchVal.setErrors(null);
+             } else {
+              this.isInValidOrgSelection = true
+            orgSearchVal.setErrors({ invalidSelection: true });
+             }
           } else {
             this.deptFilterData = this.departmentData
           }
         })
     }
 
+    
+
     if (this.transferRequestForm.get('designation')) {
+      console.log(this.transferRequestForm.get('designation')!.valueChanges, "this.transferRequestForm.get('designation')!.valueChanges")
+      
       this.transferRequestForm.get('designation')!.valueChanges
         .pipe(
-          debounceTime(250),
+          debounceTime(250),  
           distinctUntilChanged(),
           startWith(''),
         )
         .subscribe(res => {
           if (res) {
+            console.log(res, "res==============")
             const designonData = this.data && this.data.designationsMeta
             this.designationData = designonData.filter((val: any) =>
+
               val && val.name.toLowerCase().includes(res && res.toLowerCase())
+           
+            
             )
-          } else {
-            this.designationData = this.data && this.data.designationsMeta
-          }
+            console.log(this.designationData, "this.designationData--------")
+          } 
         })
     }
+
+    // this.isValidAutocomplete()
   }
 
   ngOnInit() {
     this.getAllDeptData()
+    
   }
 
+  validateOrgAutocomplete(){
+    const orgSearchVal = this.transferRequestForm.controls['organization']
+    const orgsrch = orgSearchVal && orgSearchVal.value
+    console.log(orgSearchVal.value)
+    // const designonData = this.data && this.data.designationsMeta
+
+    
+    const isValid = this.departmentData &&
+             this.departmentData.filter(item => item.toLowerCase().includes(orgsrch && orgsrch.toLowerCase()))
+    // console.log(isValid, "isValid-------------")
+    if(isValid) {
+      // this.isNotValidOrgSelection = true
+      orgSearchVal.setErrors(null);
+    } else {
+      // this.isNotValidOrgSelection = false
+      orgSearchVal.setErrors({ invalidSelection: true });
+    }
+    // console.log(this.isNotValidOrgSelection, "this.isNotValidOrgSelection--")
+  }
+
+  isValidAutocomplete() {
+    this.validateOrgAutocomplete()
+    // console.log(this.isValidOrgSelection, "this.isValidOrgSelection======")
+    return true
+  } 
   handleCloseModal(): void {
     this.dialogRef.close()
   }
@@ -113,6 +164,10 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
   }
 
   handleSubmitRequest(): void {
+    console.log('bform submit=')
+    debugger
+    if (this.transferRequestForm.valid ) {
+
     const data: any = {
       'name': this.transferRequestForm.value['organization'],
       'designation': this.transferRequestForm.value['designation'],
@@ -142,6 +197,7 @@ export class TransferRequestComponent implements OnInit, OnDestroy {
           this.matSnackBar.open(this.handleTranslateTo('transferRequestFailed'))
         }
       })
+    }
   }
 
   getAllDeptData(): void {
