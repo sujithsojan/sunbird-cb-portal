aded19e76b1301b5db11bd70de018e7120b39c45
Vishnu Bansal <vishnubansaltarento>
2024-10-21 11:48:51 +0530

    update for sync with 18 branch

--- project/ws/app/src/lib/routes/see-all/components/see-all-home/see-all-home.component.ts
+++ project/ws/app/src/lib/routes/see-all/components/see-all-home/see-all-home.component.ts
@@ -11,7 +11,7 @@ import {
 import * as _ from 'lodash'
 import { ConfigurationsService, EventService, MultilingualTranslationsService, WsEvents, NsContent } from '@sunbird-cb/utils-v2'
 import { SeeAllService } from '../../services/see-all.service'
-import { WidgetUserService } from '@sunbird-cb/collection/src/lib/_services/widget-user.service'
+// import { WidgetUserService } from '@sunbird-cb/collection/src/lib/_services/widget-user.service'
 import { MatTabChangeEvent } from '@angular/material/tabs'
 import { NsContentStripWithTabs } from '@sunbird-cb/collection/src/lib/content-strip-with-tabs/content-strip-with-tabs.model'
 import { WidgetContentLibService, WidgetUserServiceLib } from '@sunbird-cb/consumption'
